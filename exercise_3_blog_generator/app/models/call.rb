7b03b43107fd19bcd2ccb14a6bddf0205be9a962
Ajith K <ajithkelangath12@gmail.com>
2025-12-03 15:06:58 +0530

    Clean up duplicate directories and extra scripts

--- exercise_3_blog_generator/app/models/call.rb
+++
@@ -1,5 +0,0 @@
-class Call < ApplicationRecord
-  validates :phone_number, presence: true
-  
-  # Statuses: pending, initiated, completed, failed
-end
--- exercise_3_blog_generator/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root 'calls#index'
-  resources :calls, only: [:index, :create]
-  resources :posts, only: [:index, :create]
-end
